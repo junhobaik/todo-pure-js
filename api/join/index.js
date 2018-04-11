5cb67720f26c70744af51b747c0956ee7af4ce5e
junhobaik <junhobaik@gmail.com>
2018-04-11 23:21:36 +0900

    [/join] mysql connection 코드 작성

--- api/join/index.js
+++ api/join/index.js
@@ -1,6 +1,11 @@
 const express = require('express'); 
 const router = express.Router();
 
+// mysql connection settings
+const mysql = require('mysql');
+const config = require('../../config/config.js');
+const connection = mysql.createConnection(config.mysql);
+
 router.get('/', (req, res) => {
   res.sendFile("./index.html");
 })
