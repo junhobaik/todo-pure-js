ceadba252c310be8d9e6f0558c8201d51d16348f
junhobaik <junhobaik@gmail.com>
2018-04-12 22:51:03 +0900

    /login/connect 로그인 코드 작성

--- api/login/index.js
+++ api/login/index.js
@@ -1,8 +1,29 @@
 const express = require('express'); 
 const router = express.Router();
 
+// mysql connection settings
+const mysql = require("mysql");
+const config = require("../../config/config.js");
+const connection = mysql.createConnection(config.mysql);
+
 router.get('/', (req, res) => {
   res.sendFile("./index.html");
+})
+
+router.post('/connect', (req, res) => {
+  const { id, password } = req.body;
+
+  if(id === '' || password === ''){
+    res.sendStatus(400);
+  }else {
+    connection.query(`select * from users where id='${id}'`, (err, rows) => {
+      if(!rows.length || rows[0].password !== password){
+        res.sendStatus(400);
+      }else {
+        res.redirect('/main');
+      }
+    })
+  }
 })
 
 module.exports = router;
