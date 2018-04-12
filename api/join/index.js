94984d7663d7b08e26a9eda4838736bba5fdf72a
junhobaik <junhobaik@gmail.com>
2018-04-12 22:14:47 +0900

    회원가입 post 요청  코드 작성

--- api/join/index.js
+++ api/join/index.js
@@ -13,13 +13,25 @@ router.get("/", (req, res) => {
 router.post("/add", (req, res) => {
   const { name, id, password, passwordCheck } = req.body;
 
-  const query = connection.query(
-    "insert into users set ?",
-    { name, id, password },
-    function(err, rows) {
-      res.redirect("/login");
-    }
-  );
+  if(name === '' || id === ''|| password === '' || passwordCheck === ''){
+    res.sendStatus(400);
+  }else if(password !== passwordCheck){
+    res.sendStatus(400);
+  }else {
+    const query = connection.query(
+      "insert into users set ?",
+      { name, id, password },
+      function(err, rows) {
+        if(!err){
+          res.redirect("/login");
+        }else {
+          res.sendStatus(400);
+        }
+      }
+    );
+  }
+
+
 });
 
 module.exports = router;
