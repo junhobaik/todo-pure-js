8ed4a12e32dca5de783bab6627b4be21bd5ae48b
junhobaik <junhobaik@gmail.com>
2018-04-12 22:13:10 +0900

    개발용 코드 추가 및 test 환경에서의 morgan 출력 변경

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const express = require('express'); 
-const app = express(); 
-const bodyParser = require('body-parser');
-const mysql = require('mysql'); 
-const morgan = require('morgan');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
-const login = require('./api/login');
-const join = require('./api/join');
-const main = require('./api/main');
+const login = require("./api/login");
+const join = require("./api/join");
+const main = require("./api/main");
 
-app.use(express.static('api'));
-app.use(morgan('dev'));
+app.use(express.static("api"));
+if(process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/api/index.html");
-})
+});
 
-app.use('/login', login);
-app.use('/join', join);
-app.use('/main', main);
+/*Development Codes************************** */
+const mysql = require("mysql");
+const config = require("./config/config.js");
+const connection = mysql.createConnection(config.mysql);
+
+app.get("/getall", (req, res) => {
+  const query = connection.query("select * from users", function(err, rows) {
+    res.send(rows);
+  });
+});
+
+app.get("/deleteall", (req, res) => {
+  const query = connection.query("truncate table users", function(err, rows) {
+    res.end();
+  });
+});
+/**************************************** */
+
+app.use("/login", login);
+app.use("/join", join);
+app.use("/main", main);
 
 module.exports = app;
