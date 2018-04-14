1e8af0aa3d13918355b4344177fd19a494ea157f
junhobaik <junhobaik@gmail.com>
2018-04-15 01:27:42 +0900

    config 템플릿에 db 정보 추가

--- config/_config.js
+++ config/_config.js
@@ -9,3 +9,24 @@ const mysql = {
 };
 
 module.exports = { mysql };
+
+/*
+describe users;
++----------+--------------+------+-----+---------+----------------+
+| Field    | Type         | Null | Key | Default | Extra          |
++----------+--------------+------+-----+---------+----------------+
+| num      | int(11)      | NO   | PRI | NULL    | auto_increment |
+| name     | varchar(128) | NO   |     | NULL    |                |
+| id       | varchar(128) | NO   | UNI | NULL    |                |
+| password | char(41)     | NO   |     | NULL    |                |
++----------+--------------+------+-----+---------+----------------+
+
+describe todos;
++----------+--------------+------+-----+---------+----------------+
+| Field    | Type         | Null | Key | Default | Extra          |
++----------+--------------+------+-----+---------+----------------+
+| user_num | int(11)      | NO   |     | NULL    |                |
+| text_num | int(11)      | NO   | PRI | NULL    | auto_increment |
+| text     | varchar(128) | YES  |     | NULL    |                |
++----------+--------------+------+-----+---------+----------------+
+*/
